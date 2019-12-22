e58b2e400fe92a3dea331bf94c228863c839c805
masaaki1915 <masaaki1915@gmail.com>
2019-12-22 14:13:43 +0900

    refactor App.js

--- App.js
+++ App.js
@@ -5,12 +5,10 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
   Dimensions,
 } from 'react-native';
 
 import MapView from 'react-native-maps';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Geolocation from '@react-native-community/geolocation';
 var {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -21,14 +19,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: '',
-      location: '',
-      region: {
-        latitude: '',
-        longitude: '',
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
+      coords: null,
     };
   }
 
@@ -36,146 +27,77 @@ class App extends React.Component {
     Geolocation.clearWatch(this.watchID);
   }
 
-  onRegionChange(region) {
-    this.setState({region});
-  }
-
   async componentDidMount() {
-    //    const location = await this.getCurrentPos(10000).catch(error => {
-    //      this.setState({error});
-    //    });
-    //    this.setState({location});
-
-    Geolocation.getCurrentPosition(
-      position => {
-        const initialRegion = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        };
-        this.setState({
-          region: initialRegion,
-        });
-        this.initialRegion = initialRegion;
-      },
-      error => alert(error.message),
-      {enableHighAccuracy: true, timeout: 2000, maximumAge: 0},
-    );
+    const {coords} = await this.getCurrentPos().catch(error => {
+      alert(error.message);
+    });
+    this.setState({coords});
 
     this.watchID = Geolocation.watchPosition(
       position => {
-        const newRegion = {
-          latitude: position.coords.latitude + 0.0004,
-          longitude: position.coords.longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        };
-
-        this.onRegionChange(newRegion);
+        this.setState({coords: position.coords});
       },
-      error => console.log(error),
-      {distanceFilter: 1},
+      error => alert(error.message),
+      {enableHighAccuracy: true, distanceFilter: 1},
     );
   }
 
-  getCurrentPos(timeout = 5000) {
+  getCurrentPos() {
     return new Promise((resolve, reject) => {
       Geolocation.getCurrentPosition(resolve, reject, {
-        timeout,
         enableHighAccuracy: true,
       });
     });
   }
 
   render() {
-    const {location, error, region} = this.state;
-    //    if (location) {
-    //      const LATITUDE = location.coords.latitude;
-    //      const LONGITUDE = location.coords.longitude;
+    const {coords} = this.state;
     return (
-      <View style={{flex: 1}} testID="home">
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={{flex: 1}}>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            <View style={styles.body}>
-              {!!region && (
-                <View style={{flex: 1}}>
-                  <MapView
-                    style={styles.map}
-                    initialRegion={this.initialRegion}
-                    region={region}
-                    onRegionChange={this.onRegionChange.bind(this)}>
-                    <MapView.Marker
-                      image={require('./map_pin_car.png')}
-                      coordinate={{
-                        latitude: region.latitude,
-                        longitude: region.longitude,
-                      }}
-                    />
-                  </MapView>
-                  <View style={{flex: 1}}>
-                    <Text>{`location: ${JSON.stringify(region)}`}</Text>
-                    {Object.keys(region).map(key => {
-                      return (
-                        <Text key={key}>
-                          {key} : {region[key]}
-                        </Text>
-                      );
-                    })}
-                  </View>
-                </View>
-              )}
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      </View>
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          {coords && (
+            <MapView
+              style={styles.map}
+              region={{
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+              }}>
+              <MapView.Marker
+                image={require('./map_pin_car.png')}
+                coordinate={{
+                  latitude: coords.latitude + 0.0003,
+                  longitude: coords.longitude,
+                }}
+              />
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  opacity: 0.8,
+                }}>
+                <Text>{`coords: {`}</Text>
+                {Object.keys(coords).map(key => {
+                  return <Text key={key}>{`  ${key} : ${coords[key]}`}</Text>;
+                })}
+                <Text>{`}`}</Text>
+              </View>
+            </MapView>
+          )}
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    backgroundColor: '#fff',
   },
   map: {
-    width: '100%',
-    height: 700,
+    flex: 1,
+    width,
+    height,
   },
 });
 
