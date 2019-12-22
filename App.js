f33d552c378a09f7f0a29e39be7fb356817ecfa2
masaaki1915 <masaaki1915@gmail.com>
2019-12-22 15:31:14 +0900

    add Hooks

--- App.js
+++ App.js
@@ -1,104 +1,10 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Dimensions,
-} from 'react-native';
 
-import MapView from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation';
-var {width, height} = Dimensions.get('window');
-const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.01;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+import CurrentUserMapView from './CurrentUserMapView';
+import TrackUserMapView from './TrackUserMapView';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      coords: null,
-    };
-  }
-
-  componentWillUnmount() {
-    Geolocation.clearWatch(this.watchID);
-  }
-
-  async componentDidMount() {
-    const {coords} = await this.getCurrentPos().catch(error => {
-      alert(error.message);
-    });
-    this.setState({coords});
-
-    this.watchID = Geolocation.watchPosition(
-      position => {
-        this.setState({coords: position.coords});
-      },
-      error => alert(error.message),
-      {enableHighAccuracy: true, distanceFilter: 1},
-    );
-  }
-
-  getCurrentPos() {
-    return new Promise((resolve, reject) => {
-      Geolocation.getCurrentPosition(resolve, reject, {
-        enableHighAccuracy: true,
-      });
-    });
-  }
-
-  render() {
-    const {coords} = this.state;
-    return (
-      <SafeAreaView style={{flex: 1}}>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          {coords && (
-            <MapView
-              style={styles.map}
-              region={{
-                latitude: coords.latitude,
-                longitude: coords.longitude,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA,
-              }}>
-              <MapView.Marker
-                image={require('./map_pin_car.png')}
-                coordinate={{
-                  latitude: coords.latitude + 0.0003,
-                  longitude: coords.longitude,
-                }}
-              />
-              <View
-                style={{
-                  backgroundColor: '#fff',
-                  opacity: 0.8,
-                }}>
-                <Text>{`coords: {`}</Text>
-                {Object.keys(coords).map(key => {
-                  return <Text key={key}>{`  ${key} : ${coords[key]}`}</Text>;
-                })}
-                <Text>{`}`}</Text>
-              </View>
-            </MapView>
-          )}
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: '#fff',
-  },
-  map: {
-    flex: 1,
-    width,
-    height,
-  },
-});
+const App = () => {
+  return <CurrentUserMapView />;
+};
 
 export default App;
---
+++ CurrentUserMapView.tsx
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, View, Text, Dimensions, StyleSheet} from 'react-native';
+
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
+
+import MapView, {Marker} from 'react-native-maps';
+
+var {width, height} = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const CurrentUserMapView = () => {
+  const [position, setPosition] = useState<GeolocationResponse['coords']>({
+    latitude: 0,
+    longitude: 0,
+    accuracy: 0,
+    altitude: 0,
+    altitudeAccuracy: 0,
+    heading: 0,
+    speed: 0,
+  });
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => setPosition(position.coords),
+      err => alert(err.message),
+      {enableHighAccuracy: true, timeout: 10000},
+    );
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {position && (
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: position.latitude,
+            longitude: position.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}>
+          <Marker
+            image={require('./map_pin_car.png')}
+            coordinate={{
+              latitude: position.latitude + 0.0003,
+              longitude: position.longitude,
+            }}
+          />
+          <View style={styles.debugContainer}>
+            <Text>{`coords: {`}</Text>
+            {Object.keys(position).map(key => {
+              return <Text key={key}>{`  ${key} : ${position[key]}`}</Text>;
+            })}
+            <Text>{`}`}</Text>
+          </View>
+        </MapView>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+  debugContainer: {
+    backgroundColor: '#fff',
+    opacity: 0.8,
+  },
+});
+
+export default CurrentUserMapView;
